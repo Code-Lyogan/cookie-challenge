ae77f8646e54ecef089a805ffaf816ce91326124
Code-Lyogan <logantrout1@hotmail.com>
2022-09-19 00:37:56 -0400

    first commit

---
+++ cookie.js
@@ -0,0 +1,9 @@
+let cookies = 0;
+let cookiesPerClick = 1;
+let clickerUpgradePrice = 50;
+let amountOfCookies = document.querySelector(".cookie-amount");
+let newClickerUpgradePrice = document.querySelector(".clicker-price")
+
+document.querySelector(".cookie.jpg").addEventListener("click", fucntion() {
+    cookies += cookiesPerClick +
+})
